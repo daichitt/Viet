be4b5d17839305bcb230746dc0255366b27a9feb
daichitt <gene.sixteen.000925@outlook.jp>
2021-07-24 11:17:15 +0900

    Article モデルとController追加 対応

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ArticleController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','App\Http\Controllers\ArticleController@index');
