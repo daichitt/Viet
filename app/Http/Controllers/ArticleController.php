1a89de5fcb1e0e9949a46e47bacc98db1ecd7646
daichitt <gene.sixteen.000925@outlook.jp>
2021-08-07 12:19:09 +0900

    記事投稿機能 対応

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Article;
+use App\models\Article;
 use Illuminate\Http\Request;
+
+use App\Http\Requests\ArticleRequest;
 
 class ArticleController extends Controller
 {
@@ -13,4 +15,31 @@ class ArticleController extends Controller
         $articles = Article::all()->sortByDesc('created_at');
         return view('articles.index', ['articles' => $articles]);
     }
+
+    //==========ここから追加========== 
+    public function create()
+    {
+        return view('articles.create');    
+    }
+    //==========ここまで追加==========
+
+
+    //==========ここから追加========== 
+    public function edit(Article $article)
+    {
+        return view('articles.edit', ['article' => $article]);    
+    }
+    //==========ここまで追加==========
+
+    //==========ここから追加==========
+    public function store(ArticleRequest $request, Article $article)
+    {
+        // $article->title = $request->title;
+        // $article->body = $request->body;
+        $article->fill($request->all()); //-- この行を追加
+        $article->user_id = $request->user()->id;
+        $article->save();
+        return redirect()->route('articles.index');
+    }
+    //==========ここまで追加==========
 }
